f11606fc4c01fa3bc11898998958d156beb8cff1
EAM26 <emuller2607@gmail.com>
2024-08-07 21:34:51 +0200

    Create basic GUI window, lesson 154

--- functions.py
+++ functions.py
@@ -1,4 +1,7 @@
-def get_todos(filepath='todos.txt'):
+FILEPATH = 'todos.txt'
+
+
+def get_todos(filepath=FILEPATH):
     """"
     Read s text file and return a
     list of to-do items
@@ -7,10 +10,14 @@ def get_todos(filepath='todos.txt'):
         return file.readlines()
 
 
-def write_todos(content, filepath='todos.txt'):
+def write_todos(content, filepath=FILEPATH):
     """
     Open or create a textfile and write
      to-do items in it
      """
     with open(filepath, 'w') as file:
         return file.writelines(content)
+
+
+if __name__ == "__main__":
+    print("This is the functions module running as main")
