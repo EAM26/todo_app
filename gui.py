06e447669bba58810a97dec5c86ae2d18e15a410
EAM26 <emuller2607@gmail.com>
2024-08-08 13:25:32 +0200

    implement an Edit button

--- gui.py
+++ gui.py
@@ -1,25 +1,42 @@
 import functions
 import FreeSimpleGUI as sg
 
+
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_todos(), key='todos',
+                      enable_events=True, size=(45, 10))
+edit_button = sg.Button("Edit")
 exit_button = sg.Button("Exit")
 
 window = sg.Window('My To-Do App',
-                   layout=[[label], [input_box, add_button], [exit_button]],
+                   layout=[[label], [input_box, add_button], [list_box, edit_button], [exit_button]],
                    font=('Helvetica', 20))
 
 while True:
     event, value = window.read()
     print(event)
     print(value)
+    print(value['todos'])
 
     match event:
         case 'Add':
             todos = functions.get_todos()
             todos.append(value['todo'].title() + '\n')
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case 'Edit':
+            old_value = value['todos'][0]
+            todos = functions.get_todos()
+            index = todos.index(old_value)
+            todos[index] = value['todo'].title() + '\n'
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case 'todos':
+            window['todo'].update(value=value['todos'][0].strip('\n'))
 
         case 'Exit':
             break
