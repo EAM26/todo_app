6210f04801b0cc44f9931cfe430782ab5e24f11d
EAM26 <emuller2607@gmail.com>
2024-08-09 16:44:51 +0200

    Add styling buttons

--- gui.py
+++ gui.py
@@ -5,11 +5,13 @@ sg.theme("PythonPlus")
 label_clock = sg.Text("", key='clock')
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
-add_button = sg.Button("Add")
+add_button = sg.Button(key="Add",image_source='add.png', image_size=(25, 25),
+                       mouseover_colors="Orange", tooltip="Add Todo")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=(45, 10))
 edit_button = sg.Button("Edit")
-complete_button = sg.Button("Complete")
+complete_button = sg.Button(key="Complete", image_source='complete.png',
+                            mouseover_colors="Orange", tooltip="Complete Todo")
 exit_button = sg.Button("Exit")
 layout = [[label_clock], [label],
           [input_box, add_button],
